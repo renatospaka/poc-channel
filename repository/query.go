55fd779f9d43270a9789fcd86bd11e2d1e2efb4d
Renato Spakauskas <renatospaka@gmail.com>
2023-10-22 00:54:26 -0300

    refactor: :recycle: working with logs

--- repository/query.go
+++ repository/query.go
@@ -10,26 +10,25 @@ var data *Transactions
 func ProcessInLine() (*Transactions, error) {
 	processed := &Transactions{}
 	now := time.Now()
-	data = NewTransactions(20)
+	data = NewTransactions(50)
 
-	// processed.ToProcess = append(processed.ToProcess, data.ToProcess...)
-	for x := 0; x<len(data.ToProcess);x++ {
+	for x := 0; x < len(data.ToProcess); x++ {
 		processed.ToProcess = append(processed.ToProcess, data.ToProcess[x])
-		log.Printf("in-line at Repository ==> ID: %d | Parcela: %d | Valor: %f | Descrição: %s\n", processed.ToProcess[x].ID, processed.ToProcess[x].Parcelas, processed.ToProcess[x].Valor, processed.ToProcess[x].Descricao)
+		log.Printf("in-line at Repository ==> ID: %d | Parcelas: %d | Valor: %f | Descrição: %s\n", processed.ToProcess[x].ID, processed.ToProcess[x].Parcelas, processed.ToProcess[x].Valor, processed.ToProcess[x].Descricao)
 	}
 	log.Printf("in-line at Repository: %s for %d items\n", time.Since(now).String(), len(processed.ToProcess))
 	log.Println()
 	return processed, nil
 }
 
-func ProcessChannel(chTransaction chan *Transaction) error {
+func ProcessChannel(chTransaction chan<- *Transaction) error {
 	processed := 0
 	now := time.Now()
-	data = NewTransactions(20)
+	data = NewTransactions(50)
 
 	for ch := 0; ch < len(data.ToProcess); ch++ {
 		chTransaction <- data.ToProcess[ch]
-		log.Printf("channel at Repository ==> ID: %d | Parcela: %d | Valor: %f | Descrição: %s\n", data.ToProcess[processed].ID, data.ToProcess[processed].Parcelas, data.ToProcess[processed].Valor, data.ToProcess[processed].Descricao)
+		log.Printf("channel at Repository ==> ID: %d | Parcelas: %d | Valor: %f | Descrição: %s\n", data.ToProcess[processed].ID, data.ToProcess[processed].Parcelas, data.ToProcess[processed].Valor, data.ToProcess[processed].Descricao)
 		processed++
 	}
 	close(chTransaction)
--- usecase/usecase.go
+++ usecase/usecase.go
@@ -8,36 +8,32 @@ import (
 )
 
 func ProcessInLine() error {
-	now := time.Now()
 	log.Println("in-line at usecase")
-
+	now := time.Now()
 	toProcess, _ := repository.ProcessInLine()
 	newTP := repository.Transactions{}
-	for x := 0; x<len(toProcess.ToProcess);x++ {
+
+	for x := 0; x < len(toProcess.ToProcess); x++ {
 		newTP.ToProcess = append(newTP.ToProcess, toProcess.ToProcess[x])
-		log.Printf("in-line at Usecase ==> ID: %d | Parcela: %d | Valor: %f | Descrição: %s\n", toProcess.ToProcess[x].ID, toProcess.ToProcess[x].Parcelas, toProcess.ToProcess[x].Valor, toProcess.ToProcess[x].Descricao)
+		log.Printf("in-line at usecase ==> ID: %d | Parcelas: %d | Valor: %f | Descrição: %s\n", toProcess.ToProcess[x].ID, toProcess.ToProcess[x].Parcelas, toProcess.ToProcess[x].Valor, toProcess.ToProcess[x].Descricao)
 	}
-	
-	log.Printf("in-line at Usecase: %s for %d items\n", time.Since(now).String(), len(newTP.ToProcess))
+
+	log.Printf("in-line at usecase: %s for %d items\n", time.Since(now).String(), len(newTP.ToProcess))
 	return nil
 }
 
 func ProcessChannel() error {
-	processed := 0
-	now := time.Now()
-	toProcess := make(chan *repository.Transaction)
 	log.Println("channel at usecase")
+	now := time.Now()
+	processed := 0
+	toProcess := make(chan *repository.Transaction)
 
 	go repository.ProcessChannel(toProcess)
 	for t := range toProcess {
 		processed++
-		
-		log.Printf("channel at Usecase ==> ID: %d | Parcela: %d | Valor: %f | Descrição: %s\n", t.ID, t.Parcelas, t.Valor, t.Descricao)
-		// if processed < 2 {
-		// 	log.Printf("ID: %d\n", t.ID)
-		// }		
+		log.Printf("channel at usecase ==> ID: %d | Parcelas: %d | Valor: %f | Descrição: %s\n", t.ID, t.Parcelas, t.Valor, t.Descricao)
 	}
-	
-	log.Printf("channel at Usecase: %s for %d items\n", time.Since(now).String(), processed)
+
+	log.Printf("channel at usecase: %s for %d items\n", time.Since(now).String(), processed)
 	return nil
 }
